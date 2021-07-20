158b19d369ee86addd9dd7a722ced7b36a6af332
ottagit <ottacc66@gmail.com>
2021-07-20 22:02:13 +0300

    set up cucumber for Java BDD

---
+++ src/test/java/stepdefinitions/TestRunner.java
@@ -0,0 +1,18 @@
+package stepdefinitions;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/features",
+        glue={"stepdefinitions"},
+        monochrome = true,
+        plugin = {"pretty", "html:target/html-reports/report.html",
+                "json:target/json-reports/report.json",
+                "junit:target/xml-reports/report.xml"
+        },
+        tags = "@smoketest")
+public class TestRunner {
+
+}
